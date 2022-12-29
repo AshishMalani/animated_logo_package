f8dcf58cbd2a66d1ac9e2224060a9cbbd528e5a2
Ashish Malani <115336426+AshishMalani@users.noreply.github.com>
2022-12-29 10:16:45 +0530

    initial commit

---
+++ lib/animated_logo.dart
@@ -0,0 +1,48 @@
+library animated_logo;
+
+import 'package:flutter/material.dart';
+
+class AnimatedLogo extends StatefulWidget {
+  const AnimatedLogo({Key? key, required this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  State<AnimatedLogo> createState() => _AnimatedLogoState();
+}
+
+class _AnimatedLogoState extends State<AnimatedLogo>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 5000),
+    );
+    animationController.forward();
+    animationController.addListener(() {
+      setState(() {
+        if (animationController.status == AnimationStatus.completed) {
+          animationController.repeat();
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animationController,
+      child: widget.child,
+      builder: (context, child) {
+        return Transform.rotate(
+          angle: animationController.value,
+          child: child,
+        );
+      },
+    );
+  }
+}
